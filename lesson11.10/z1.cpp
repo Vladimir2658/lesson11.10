ad9f8847265515a12e4661371a53e9295368b6ee
ytoneex@gmail.com <ytoneex@gmail.com>
2023-10-11 21:28:34 +0300

    rek

--- lesson11.10/z1.cpp
+++ lesson11.10/z1.cpp
@@ -1,8 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-
 using namespace std;
-
-int main() {
-	artemiy lox nu tochno lox otpravi levi emy 2 semki
-	return 0;
+int main()
+{
+	char s[30];
+	FILE* out;
+	const char* path = "infile.txt";
+	if ((fopen_s(&out,path, "w+"))!=NULL) {
+		cout << "The file could not be created!";
+	}
+	else {
+		while (!feof(out)) {
+			fscanf_s(out, "%s", s, sizeof(s));
+			cout << s;
+}
+		cout << "OK!";
+		fprintf(out, "привет");
+	}	return 0;
 }
