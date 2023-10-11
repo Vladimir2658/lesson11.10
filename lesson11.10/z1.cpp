d7aaa269e1a32b0f672461f6fd83c6e9e5f86aa2
Zimunchik <zimunchik@yandex.ru>
2023-10-11 20:31:58 +0300

    file

--- lesson11.10/z1.cpp
+++ lesson11.10/z1.cpp
@@ -3,5 +3,12 @@
 using namespace std;
 
 int main() {
+	FILE* out;
+	if ((out = fopen("outfile.txt", "w+") == NULL) {
+		cout << "File could'nt be created!";
+	}
+	else {
+		cout << "OK!";
+	}
 	return 0;
 }
